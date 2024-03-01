efe45e14da62f9209b315ebc0d4a871fc871a6f7
trannhusum <147652638+trannhusum@users.noreply.github.com>
2024-03-01 14:37:49 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai3_w06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student.Change();//calling change method
+                             //creating objects
+            student s1 = new student(111, "Hoang");
+            student s2 = new student(222, "Khanh");
+            student s3 = new student(333, "Nam");
+            //calling display method
+            s1.Display();
+            s2.Display();
+            s3.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace bai3_w06
+{
+    public class student
+    {
+        private int rollno;
+        private string name;
+        private static string college = "BBDIT";    
+        public student(int r, string n)
+        {
+            rollno = r;
+            name = n;
+        }
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+        public void Display()
+        {
+            Console.WriteLine(rollno + " " + name + " " + college);
+        }
+    }
+}
